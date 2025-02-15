3b036b9eaee4fea604dfa0921e0bd105d7313962
Lafaiete <lafaiete.machado@gmail.com>
2025-02-15 12:52:31 -0300

    Integração com Banco de Dados

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,49 @@
 import { test, expect } from '@playwright/test';
 
-test('Nao deve logar quando o codigo de autenticacao e invalido', async ({ page }) => {
-  
+import { obterCodigo2FA } from '../support/db';
+
+import { LoginPage } from '../pages/LoginPage';
+
+import { DashPage } from '../pages/DashPage';
+
+test('Não deve logar quando o codigo de autenticacao é invalido', async ({ page }) => {
+  const loginPage = new LoginPage(page)
   const usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+  await loginPage.informe2FA('123456')
   
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+  
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+
+  // temporário
+  await page.waitForTimeout(3000)
+  const codigo = await obterCodigo2FA()
+
+  await loginPage.informe2FA(codigo)
+  
+  // temporário
+  await page.waitForTimeout(2000)
+
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+
 });
